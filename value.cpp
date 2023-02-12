158b8982fe551924584206b667e395ab55a8725c
Swajan Baruah <70163983+swajan-75@users.noreply.github.com>
2023-02-12 12:33:06 +0600

    Compare & Shifting count For selection Sort

--- value.cpp
+++ value.cpp
@@ -1,36 +1,31 @@
-#include<iostream>
-using namespace std; 
-void InsertionSort(int arr[],int n){
-    int startingIndex , elementOnHand , currentIndex  ;
-    int shift =0  , comapare=0 , k =0;
-    for(startingIndex = 1 ; startingIndex <n ;startingIndex++){
-        elementOnHand = arr[startingIndex];
-        currentIndex = startingIndex -1 ;
-       if(arr[currentIndex]<elementOnHand){
-            k++;
-            //comapare++ ;
-       }
-        comapare++ ;//n-1n
-        while(currentIndex>=0 && arr[currentIndex]>elementOnHand){
-            arr[currentIndex+1]=arr[currentIndex];
-            currentIndex = currentIndex-1 ;
-            shift++ ;
-            comapare++ ;
-            
-            
+//Compare & shifting Count For Selection Sort 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int size = 10 ;
+    int shift = 0 ,comp=0 ;
+    int arr[]={33,66,22,99,77,11,91,55,88,44}; ;
+    int min ,temp ;
+    for(int i =0 ;i <size-1 ;i++){
+        min = i ;
+        for(int j =i +1;j<size;j++){
+            comp++ ;
+            if(arr[min]>arr[j]){
+                min = j ;
+                
+            }
         }
-        arr[currentIndex+1]=elementOnHand ;
-        //int arr[]={33,66,22,99,77,11,91,55,88,44};cout<<k<<" ";
-        //k =0 ;
+        if(i!=min){
+            shift++;
+        }
+        temp = arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+        
     }
-    for(int i =0 ;i <n;i++){
+    for(int i =0 ;i<size;i++){
         cout<<arr[i]<<" ";
     }
-    cout<<endl<<shift<<" "<<comapare-k;
-}
-int main(){
-    int arr[]={10,9,8,7,6,5,4,3,2,1};
-    //int arr[]={33,66,22,99,77,11,91,55,88,44};
-    InsertionSort(arr ,(sizeof(arr)/sizeof(arr[0])));
-    
+    cout<<endl<<shift<<" "<<comp ;
+
 }
